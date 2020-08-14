597e0a8b8259a7da61645d38d16636eb9416d447
noelma <noel-mathieu@hotmail.fr>
2020-08-14 15:03:21 +0200

    #85 Nouvelle règle is_file pour le Validator.

--- tests/Components/Validator/Rules/IsFileTest.php
+++ tests/Components/Validator/Rules/IsFileTest.php
@@ -2,30 +2,30 @@
 
 namespace Soosyze\Tests\Components\Validator\Rules;
 
-class DirTest extends Rule
+class IsFileTest extends Rule
 {
-    public function testDir()
+    public function testIsFile()
     {
         $this->object->setInputs([
-            'must'              => __DIR__,
-            'not_must'          => 'not dir',
-            'required_must'     => __DIR__,
+            'must'              => __DIR__ . '/IsFileTest.php',
+            'not_must'          => 'not file',
+            'required_must'     => __DIR__ . '/IsFileTest.php',
             'not_required_must' => ''
         ])->setRules([
-            'must'              => 'dir',
-            'not_must'          => '!dir',
-            'required_must'     => 'required|dir',
-            'not_required_must' => '!required|dir'
+            'must'              => 'is_file',
+            'not_must'          => '!is_file',
+            'required_must'     => 'required|is_file',
+            'not_required_must' => '!required|is_file'
         ]);
 
         $this->assertTrue($this->object->isValid());
 
         $this->object->setInputs([
-            'must'     => 'not dir',
-            'not_must' => __DIR__
+            'must'     => 'not file',
+            'not_must' => __DIR__ . '/IsFileTest.php',
         ])->setRules([
-            'must'     => 'dir',
-            'not_must' => '!dir'
+            'must'     => 'is_file',
+            'not_must' => '!is_file'
         ]);
 
         $this->assertFalse($this->object->isValid());
