f3d03e11a3da27f2eebd6f1ed1ff4756f9472ae6
noelma <noel-mathieu@hotmail.fr>
2021-02-22 21:15:11 +0100

    Revue de code, déclaration des fonctions anonymes en static.

--- src/Components/Router/Router.php
+++ src/Components/Router/Router.php
@@ -134,7 +134,7 @@ class Router
      */
     public function getRegexForPath($path, array $param)
     {
-        array_walk($param, function (&$with) {
+        array_walk($param, static function (&$with) {
             $with = str_replace([ '(', '/' ], [ '(?:', '\/' ], $with);
             $with = "($with)";
         });
